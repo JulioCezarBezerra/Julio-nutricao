518ac0757f43f5a307fe8ccdef90d8301824d420
Júlio Bezerra <juliobezerradev@outlook.com>
2022-11-30 22:44:54 -0300

    commit

---
+++ js/buscar-pacientes.js
@@ -0,0 +1,8 @@
+var botaoAdicionar = document.querySelector("#buscar-pacientes");
+
+botaoAdicionar.addEventListener("click", function() {
+    console.log("buscando");
+
+    var xhr = new XMLHttpRequest();
+    
+})
--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,7 +1,11 @@
 var pacientes = document.querySelectorAll(".paciente");
 
-pacientes.forEach(function(paciente){
-    paciente.addEventListener("dblclick", function(){
-        console.log("fui clicado 2x")
-    })
+var tabela = document.querySelector("#tabela-pacientes");
+
+tabela.addEventListener("dblclick",function(event){
+    event.target.parentNode.classList.add("fadeOut");
+
+    setTimeout(function(){
+        event.target.parentNode.remove();
+    }, 500)
 });
